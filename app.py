563028c3f84100ad4b543f5852dc972f3577fc85
RC Keely <rckeely@gmail.com>
2019-11-25 20:45:39 +0100

    Integrated table callback with tabs

--- app.py
+++ app.py
@@ -44,16 +44,16 @@ app.layout = \
         dbc.Row([
             dbc.Col([
                 html.Div(className="sidePanel", children=[
-                    html.Div(className="subcomponent", children=[
-                        html.Button('Optimise!', id='button'),
-                        html.Br(),
-                    ]),
+                    # html.Div(className="subcomponent", children=[
+                    #     html.Button('Optimise!', id='button'),
+                    #     html.Br(),
+                    # ]),
                     html.Div(className="subcomponent", children=[
                         html.H3('Enter current league week:', className="sidePanel"),
                         dcc.Dropdown(
                             id='current_week',
-                            options=[{'label': str(i), 'value': int(i)} for i in weeks],
-                            value=int(1),
+                            options=[{'label': str(i), 'value': i} for i in weeks],
+                            value='',
                             placeholder="Current week",
                             multi=False
                         ),
@@ -72,7 +72,7 @@ app.layout = \
                     ]),
                     html.Div(className="subcomponent", children=[
                         html.H3('Select Teams:', className="sidePanel"),
-                        dcc.Checklist(id='teams',
+                        dcc.Checklist(id='blocked_teams',
                                       options=[{'label': str(i), 'value': i} for i in teams],
                                       value=[]
                                      ),
@@ -82,7 +82,14 @@ app.layout = \
             ]),
             dbc.Col([
                 html.Div(className="mainPanel", children=[
-                	html.Div(id='main_table')
+                	html.Div([
+                    	dcc.Tabs(id="tabs-example", value='team_selector', children=[
+                            dcc.Tab(label='Team Selector', value='team_selector'),
+                            dcc.Tab(label='Probabilities Table', value='probabilities_table'),
+                            dcc.Tab(label='Projections Graph', value='projections_graph'),
+                        ]),
+                        html.Div(id='tabs-content-example')
+                	])
             	]),
             ])
         ]),
@@ -90,16 +97,62 @@ app.layout = \
                                 children=[html.H1("Footer",
                                                 className="footer")]))),
     ])
-# @app.callback(
-#     Output('mainTable', 'children'),
-#     [Input('current_week', 'value')]
-# )
 
-@app.callback(Output('main_table', 'children'),
-              [Input('current_week', 'value')])
-def update_weeks(week_start):
+def fix_strings(string_var, start=False):
+    if string_var == '':
+        if start:
+            string_var = 1
+        else:
+            string_var = 17
+    else:
+        string_var = int(string_var)
+    return string_var
+
+
+def get_table_div(week_start, week_end, blocked_teams):
+    week_start = fix_strings(week_start, start=True)
+    week_end = fix_strings(week_end)
+    results_df = optimize_season(mydata, week_start=week_start,
+                                 week_end=week_end, blocked_teams=blocked_teams)
     return html.Div(
-                dbc.Table.from_dataframe(df=optimize_season(mydata, week_start=week_start, week_end=17, blocked_teams=blocked_teams),
+                dbc.Table.from_dataframe(df=results_df,
                                     id="mainTable"))
+
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value'),
+               Input('current_week', 'value'),
+               Input('target_week', 'value'),
+               Input('blocked_teams', 'value')])
+def render_content(tab, week_start, week_end, blocked_teams):
+    if tab == 'team_selector':
+        return html.Div([
+            html.H3('Tab content 1'),
+            dcc.Graph(
+                id='graph-1-tabs',
+                figure={
+                    'data': [{
+                        'x': [1, 2, 3],
+                        'y': [3, 1, 2],
+                        'type': 'bar'
+                    }]
+                }
+            )
+        ])
+    elif tab == 'probabilities_table':
+        return get_table_div(week_start, week_end, blocked_teams)
+    elif tab == 'projections_graph':
+        return html.Div([
+            html.H3('Tab content 3'),
+            dcc.Graph(
+                id='graph-3-tabs',
+                figure={
+                    'data': [{
+                        'x': [1, 2, 3],
+                        'y': [5, 10, 6],
+                        'type': 'bar'
+                    }]
+                }
+            )
+        ])
 
 app.run_server(debug=True)
