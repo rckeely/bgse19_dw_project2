c1ecd443fa9c0579a79a328ceae28c25bc42b94b
BenCohen <cohen.benr@gmail.com>
2019-11-27 19:54:27 +0100

    Moved getting thumbnails to front, dropped headline stats, organized util_functions, and a few other cleanups

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
+#Core modules
+import pandas as pd
+import os
+
+#Dash modules
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-
-import pandas as pd
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 
-from dash.dependencies import Input, Output
-
-import os
+#Custom modules
 from optimizer import *
 from transform_elo import *
 from util_functions import *
-
-results_df = pd.DataFrame()
-
-app = dash.Dash('NFL Survivor Pool Optimiser')
-#app.config.suppress_callback_exceptions = True
-app.title = 'NFL Survivor Pool Optimiser'
-#Get data
-mydata = pd.read_csv('data/elo/nfl_elo.csv')
-teams_full = pd.read_csv('data/teams.csv')
-
-longdata = transform_elo_data(mydata)
-static_df = pd.read_csv('data/nfl_lookup_table.csv')
-teams = teams_full['abb']
-
-blocked_teams = []
-
-SEASON_LENGTH = 17
-weeks = list(range(1, SEASON_LENGTH + 1))
-target_weeks = list(range(1, SEASON_LENGTH + 1))
-
-x = generate_table_df(longdata, week_start=1,
-                      week_end=17, blocked_teams=blocked_teams)
 
 # Colours
 # Dark Grey #5d5c61
@@ -41,117 +21,134 @@ x = generate_table_df(longdata, week_start=1,
 # Light blue #7395ae
 # Dark blue #557a95
 # Sand #b1a296
-app.layout = \
-    html.Div(className="container", children=[
-        dbc.Row(dbc.Col(html.Div(className="mast_head",
-                children=[html.H1(className="mast_head",
-                                children=[html.Img(className="logo",
-                                                   src=app.get_asset_url('nfl-league-logo.png'),),
-                                         f"NFL Survivor Pool Optimiser",]),
-                        html.Hr(className="mast_head")]))),
-        dbc.Row([
-            dbc.Col([
-                html.Div(className="side_panel", children=[
-                    html.Div(className="subcomponent", children=[
-                        html.H3('Enter current league week:', className="side_panel"),
-                        dcc.Dropdown(
-                            id='current_week',
-                            options=[{'label': str(i), 'value': i} for i in weeks],
-                            value='',
-                            placeholder="Current week",
-                            multi=False,
-                            className="side_controls"
-                        ),
-                        html.Br()
-                    ]),
-                    html.Div(className="subcomponent", children=[
-                        html.H3('Optimise through week:', className="side_panel"),
-                        dcc.Dropdown(
-                            id='target_week',
-                            options=[{'label': str(i), 'value': i} for i in target_weeks],
-                            value='',
-                            placeholder='Final week',
-                            multi=False,
-                            className="side_controls"
-                        ),
-                        html.Br()
-                    ]),
-                    html.Div(className="subcomponent", children=[
-                        html.H3('Block Teams:', className="side_panel"),
-                        #dcc.Checklist(id='blocked_teams',
-                        #              options=[{'label': get_full_name(i) , 'value': i} for i in teams],
-                        #              value=[],
-                        #              className="check_list"
-                        #             ),
-                        html.Br(),
-                        dcc.Dropdown(
-                            id='blocked_teams',
-                            options=[{'label': get_full_name(i) , 'value': i} for i in teams],
-                            value='',
-                            placeholder='Select Team',
-                            multi=True,
-                            className="side_controls"
-                        ),
-                        html.Br()
-                    ]),
-                    # html.Div(className="subcomponent", children=[
-                    #     html.H3('Block Teams:', className="side_panel"),
-                    #     dcc.Checklist(id='blocked_teams',
-                    #                   options=[{'label': get_full_name(i) , 'value': i} for i in teams],
-                    #                   value=[],
-                    #                   className="check_list"
-                    #                  ),
-                    #     html.Br(),
-                    # ]),
-                ]),
-            ]),
-            dbc.Col([
-                html.Div(className="main_panel", children=[
-                	html.Div([
-                        html.Div(id="headline_stats", className="headline_stats"),
-                    	dcc.Tabs(id="tabs-example", value='probabilities_table', children=[
-                            dcc.Tab(label='Team Location', value='team_selector'),
-                            dcc.Tab(label='Probabilities Table', value='probabilities_table'),
-                            dcc.Tab(label='Projections Graph', value='projections_graph'),
+
+if __name__ == "__main__":
+    app = dash.Dash('NFL Survivor Pool Optimiser')
+    # app.config.suppress_callback_exceptions = True
+    app.title = 'BGSE NFL Survivor Pool Optimiser'
+
+    # Get data
+    mydata = pd.read_csv('data/elo/nfl_elo.csv')
+    static_df = pd.read_csv('data/nfl_lookup_table.csv')
+
+    longdata = transform_elo_data(mydata)
+
+    teams = static_df['ShortCode']
+    SEASON_LENGTH = 17
+    weeks = list(range(1, SEASON_LENGTH + 1))
+    target_weeks = list(range(1, SEASON_LENGTH + 1))
+
+    thumbnails = initialize_thumbnails(static_df)
+
+    #Initialize Params
+    blocked_teams = []
+    #current_week = 1 #for now
+    #week_start = current_week
+    #week_end = 17
+
+    #results_df = optimize_season(longdata=longdata,week_start=week_start,week_end=week_end,blocked_teams=blocked_teams)
+    results_df = pd.DataFrame()
+
+    app.layout = \
+        html.Div(className="container", children=[
+            dbc.Row(dbc.Col(html.Div(className="mast_head",
+                                     children=[html.H1(className="mast_head",
+                                                       children=[html.Img(className="logo",
+                                                                          src=app.get_asset_url(
+                                                                              'nfl-league-logo.png'), ),
+                                                                 f"NFL Survivor Pool Optimiser", ]),
+                                               html.Hr(className="mast_head")]))),
+            dbc.Row([
+                dbc.Col([
+                    html.Div(className="side_panel", children=[
+                        html.Div(className="subcomponent", children=[
+                            html.H3('Enter current league week:', className="side_panel"),
+                            dcc.Dropdown(
+                                id='current_week',
+                                options=[{'label': str(i), 'value': i} for i in weeks],
+                                value='',
+                                placeholder="Current week",
+                                multi=False,
+                                className="side_controls"
+                            ),
+                            html.Br()
                         ]),
-                        html.Div(id='tabs-content-example', className='tab_content')
-                	])
-            	]),
-            ])
-        ]),
-        dbc.Row(dbc.Col(html.Div(className="footer",
-                                children=[html.H1("Footer",
-                                                className="footer")]))),
-    ])
+                        html.Div(className="subcomponent", children=[
+                            html.H3('Optimise through week:', className="side_panel"),
+                            dcc.Dropdown(
+                                id='target_week',
+                                options=[{'label': str(i), 'value': i} for i in target_weeks],
+                                value='',
+                                placeholder='Final week',
+                                multi=False,
+                                className="side_controls"
+                            ),
+                            html.Br()
+                        ]),
+                        html.Div(className="subcomponent", children=[
+                            html.H3('Block Teams:', className="side_panel"),
+                            # dcc.Checklist(id='blocked_teams',
+                            #              options=[{'label': get_full_name(i) , 'value': i} for i in teams],
+                            #              value=[],
+                            #              className="check_list"
+                            #             ),
+                            html.Br(),
+                            dcc.Dropdown(
+                                id='blocked_teams',
+                                options=[{'label': get_full_name(i), 'value': i} for i in teams],
+                                value='',
+                                placeholder='Select Team',
+                                multi=True,
+                                className="side_controls"
+                            ),
+                            html.Br()
+                        ]),
+                        # html.Div(className="subcomponent", children=[
+                        #     html.H3('Block Teams:', className="side_panel"),
+                        #     dcc.Checklist(id='blocked_teams',
+                        #                   options=[{'label': get_full_name(i) , 'value': i} for i in teams],
+                        #                   value=[],
+                        #                   className="check_list"
+                        #                  ),
+                        #     html.Br(),
+                        # ]),
+                    ]),
+                ]),
+                dbc.Col([
+                    html.Div(className="main_panel", children=[
+                        html.Div([
+                            #html.Div(id="headline_stats", className="headline_stats"),
+                            dcc.Tabs(id="tabs-example", value='probabilities_table', children=[
+                                dcc.Tab(label='Team Location', value='team_selector'),
+                                dcc.Tab(label='Probabilities Table', value='probabilities_table'),
+                                dcc.Tab(label='Projections Graph', value='projections_graph'),
+                            ]),
+                            html.Div(id='tabs-content-example', className='tab_content')
+                        ])
+                    ]),
+                ])
+            ]),
+            dbc.Row(dbc.Col(html.Div(className="footer",
+                                     children=[html.H1("Footer",
+                                                       className="footer")]))),
+        ])
 
-def get_selector_div(longdata, week_start, week_end, blocked_teams, static_df):
-     return html.Div(className="map_div", children=[
-        dcc.Graph(
-            id='graph-1-tabs',
-            figure={
-                'data': [go.Scattergeo(
-                        lon = static_df['Longitude'],
-                        lat = static_df['Latitude'],
-                        text = static_df['TeamName'],
-                        mode = 'markers')],
-                'layout': go.Layout(geo_scope='usa',
-                            width=1000,
-                            height=600)
-                })])
+    @app.callback(Output('tabs-content-example', 'children'),
+                   #Output('headline_stats', 'children')],
+                  [Input('tabs-example', 'value'),
+                   Input('current_week', 'value'),
+                   Input('target_week', 'value'),
+                   Input('blocked_teams', 'value')])
+    def render_content(tab, week_start, week_end, blocked_teams):
+        if tab == 'team_selector':
+            result = get_selector_div(longdata, week_start, week_end, blocked_teams, static_df)
+        elif tab == 'probabilities_table':
+            result = get_table_div(longdata, results_df, week_start, week_end, blocked_teams, thumbnails)
+        elif tab == 'projections_graph':
+            result = get_projections_graph(longdata=longdata, results_df=results_df,week_start=week_start,
+                                           week_end=week_end, blocked_teams=blocked_teams, thumbnails=thumbnails)
 
-@app.callback([Output('tabs-content-example', 'children'),
-               Output('headline_stats', 'children')],
-              [Input('tabs-example', 'value'),
-               Input('current_week', 'value'),
-               Input('target_week', 'value'),
-               Input('blocked_teams', 'value')])
-def render_content(tab, week_start, week_end, blocked_teams):
-    if tab == 'team_selector':
-        result = get_selector_div(longdata, week_start,week_end, blocked_teams, static_df)
-    elif tab == 'probabilities_table':
-        result = get_table_div(longdata, results_df, week_start, week_end, blocked_teams)
-    elif tab == 'projections_graph':
-        result = get_projections_graph(longdata, results_df, week_start, week_end, blocked_teams)
-    return result, "hello"
+        return result
 
-app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
+
--- util_functions.py
+++ util_functions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from optimizer import *
 from PIL import Image
 from io import BytesIO
@@ -10,6 +12,7 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 
+### Util Functions ###
 def get_full_name(short_code):
     full_names = { "ARI" : "Arizona Cardinals",
     "ATL" : "Atlanta Falcons",
@@ -45,32 +48,48 @@ def get_full_name(short_code):
     "WAS" : "Washington ********" }
     return full_names[short_code]
 
+
 def get_thumbnail(short_code):
    i = Image.open(f"assets/{short_code}.png")
    i.thumbnail((60, 60), Image.LANCZOS)
    buff = BytesIO()
    i.save(buff, format="PNG")
    encoded_image = base64.b64encode(buff.getvalue()).decode('UTF-8')
+   print('got a thumbnail.') # for debugging
    return (html.Div(className="team_names", children=[html.Img(src='data:image/png;base64,{}'.format(encoded_image),
                             className="team_logos"),
                     get_full_name(short_code)]))
 
-def generate_table_df(longdata,week_start,week_end,blocked_teams):
-    import pandas as pd
 
+def initialize_thumbnails(static_df):
+    output = static_df[['ShortCode']]
+    thumbs = output['ShortCode'].map(get_thumbnail)
+    output.loc[:, 'Team'] = pd.Series(thumbs, index=output.index) #this is causing the "slice" warning in terminal
+    output = output.rename(columns={'ShortCode': 'team'})
+
+    return output
+
+
+def generate_table_df(longdata, week_start, week_end, blocked_teams, thumbnails = None, results_df = None):
     results_df = optimize_season(longdata, week_start=week_start,
-                week_end=week_end, blocked_teams=blocked_teams)
-    longdata = longdata[['week','team','elo']]
-    output = results_df.merge(longdata,how='left')
+                                 week_end=week_end, blocked_teams=blocked_teams)
+
+    longdata = longdata[['week', 'team', 'elo']]
+    output = results_df.merge(longdata, how='left')
     # output['image'] = output['team'].map(str) + '.png'
-    output['team'] = output['team'].map(get_thumbnail)
+    if thumbnails is None:
+        output['Team'] = output['team']
+    else:
+        output = output.merge(thumbnails, how='left')
+
     output['WinProb'] = ["{:.1%}".format(x) for x in output.prob]
     output['ELO'] = output.elo.astype('int')
 
-    output = output.rename(columns={'week':'Week','team':'Team'})
+    output = output.rename(columns={'week': 'Week'})
 
-    output = output[['Week','Team','ELO','WinProb']]
+    output = output[['Week', 'Team', 'ELO', 'WinProb']]
     return output
+
 
 def fix_strings(string_var, start=False):
     if string_var == '':
@@ -83,25 +102,40 @@ def fix_strings(string_var, start=False):
     return string_var
 
 
-def get_table_div(longdata, results_df, week_start, week_end, blocked_teams):
+### App Layout Functions ###
+def get_selector_div(longdata, week_start, week_end, blocked_teams, static_df):
+    return html.Div(className="map_div", children=[
+        dcc.Graph(
+            id='graph-1-tabs',
+            figure={
+                'data': [go.Scattergeo(
+                    lon=static_df['Longitude'],
+                    lat=static_df['Latitude'],
+                    text=static_df['TeamName'],
+                    mode='markers')],
+                'layout': go.Layout(geo_scope='usa',
+                                    width=1000,
+                                    height=600)
+            })])
+
+
+def get_table_div(longdata, results_df, week_start, week_end, blocked_teams, thumbnails):
     week_start = fix_strings(week_start, start=True)
     week_end = fix_strings(week_end)
-    results_df = generate_table_df(longdata, week_start=week_start,
-                                   week_end=week_end, blocked_teams=blocked_teams)
+    results_df = generate_table_df(longdata, week_start=week_start, week_end=week_end,
+                                   blocked_teams=blocked_teams, thumbnails=thumbnails, results_df=results_df)
     return html.Div(className="render_div", children=
                 dbc.Table.from_dataframe(df=results_df,
                                     id="main_table"))
 
 
-def get_projections_graph(longdata, results_df, week_start, week_end, blocked_teams):
+def get_projections_graph(longdata, results_df, week_start, week_end, blocked_teams,thumbnails):
     week_start = fix_strings(week_start, start=True)
     week_end = fix_strings(week_end)
 
-    if results_df.shape[0] == 0:
-        results_df = generate_table_df(longdata, week_start=week_start,
-                                       week_end=week_end, blocked_teams=blocked_teams)
+    graph_df = generate_table_df(longdata, week_start=week_start, week_end=week_end,
+                                 blocked_teams=blocked_teams, thumbnails=thumbnails,results_df=results_df)
 
-    graph_df = results_df
     graph_df['wp'] = [float(i[:(len(i)-1)]) / 100 for i in graph_df.WinProb]
 
     graph_df['Optimized'] = graph_df.wp.cumprod()
